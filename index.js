cbd47c8ee456a6f5d168f9c9148bde6366926a8e
abunaim34 <farhanadnanfarabi@gmail.com>
2024-07-17 12:57:17 +0600

    this is signup

--- index.js
+++ index.js
@@ -38,73 +38,6 @@ async function run() {
 
         const userCollection = client.db('PayNet').collection('users')
 
-
-        // const verifyToken = (req, res, next) => {
-        //     const token = req.cookies?.token
-        //     if (!token) return res.status(401).send({ message: 'unauthorized access' })
-        //     if (token) {
-        //         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        //             if (err) {
-        //                 console.log(err);
-        //                 return res.status(401).send({ message: 'unauthorized access' })
-        //             }
-        //             req.user = decoded
-        //             next()
-        //         })
-        //     }
-        // }
-
-        // token
-        // app.post('/jwt', async (req, res) => {
-        //     const user = req.body
-        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '30d' })
-        //     res.cookie('token', token, {
-        //         httpOnly: true,
-        //         secure: process.env.NODE_ENV === 'production',
-        //         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-        //     })
-        //         .send({ success: true })
-        // })
-
-        // app.get('/logOut', (req, res) => {
-        //     res.clearCookie('token', {
-        //         httpOnly: true,
-        //         secure: process.env.NODE_ENV === 'production',
-        //         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
-        //         maxAge: 0
-        //     })
-        //         .send({ success: true })
-        // })
-
-        // user related api
-        // app.post('/register', async (req, res) => {
-        //     const { email, pin, name, status, phone, role } = req.body;
-        //     const hashedPassword = await bcrypt.hash(pin, 10);
-        //     console.log(hashedPassword);
-        //     const newUser = { email, name, status, phone, role, pin: hashedPassword };
-        //     const result = await userCollection.insertOne(newUser);
-        //     res.send(result);
-        // });
-
-        // app.post('/login',  async (req, res) => {
-        //     const { email, pin } = req.body;
-        //     const user = await userCollection.findOne({ email });
-
-        //     if (!user) return res.status(400).send('User not found.');
-
-        //     const isMatch = await bcrypt.compare(pin, user.pin);
-
-        //     if (!isMatch) return res.status(400).send('Invalid credentials.');
-
-        //     const token = jwt.sign({ id: user._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-        //     res.status(200).json({ token });
-        // });
-
-        // app.get('/users', async (req, res) => {
-        //     const result = await userCollection.find().toArray()
-        //     res.send(result)
-        // })
-
         app.post('/signup', async (req, res) => {
             const { name, email, phone, pin, role, status } = req.body;
             const hashedPin = await bcrypt.hash(pin, 10);
@@ -114,10 +47,10 @@ async function run() {
             if (existingUser) {
                 return res.status(400).json({ message: 'User already exists' });
             }
+            
             if (user && await bcrypt.compare(pin, user.pin)) {
                 const token = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '135d' });
-                console.log(token);
-                res.json({ token });
+                res.json({ token, email });
                 const result = await userCollection.insertOne(user);
                 res.status(201).json({ message: 'User created', userId: result.insertedId });
             }
@@ -129,8 +62,8 @@ async function run() {
             const user = await userCollection.findOne({ email });
 
             if (user && await bcrypt.compare(pin, user.pin)) {
-                const token = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-                return res.json({ token });
+                const token = jwt.sign({ userId: user._id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '155d' });
+                return res.json({ token, email});
             } else {
                 res.status(401).json({ message: 'Invalid credentials' });
             }
